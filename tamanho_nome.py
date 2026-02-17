c51c54de3484b6161b6088cac19e13e9b6c03dbc
jhegner <jidlafehegner@gmail.com>
2026-02-17 10:02:24 -0300

    meu primeiro programa

---
+++ tamanho_nome.py
@@ -0,0 +1,5 @@
+print("😘 PROGRAMA DESCUBRA O TAMANHO DO NOME 😘")
+meu_nome = input("Digite aqui o nome: ")
+#meu_nome = "Raquel Noa Lopes dos Santos Hegner"
+print("Meu nome é: " + meu_nome)
+print(f"e o meu nome tem {len(meu_nome)} letras")
