581e447782f4a72e4871dd308df68cec0588b9de
jhegner <jidlafehegner@gmail.com>
2026-09-10 22:02:15 -0300

    add bicycle list operations and examples

---
+++ book-python/03-listas/elementos.py
@@ -0,0 +1,15 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialize']
+#print(bicycles)
+#print(bicycles[2])
+#print(bicycles[0].title())
+
+# Ultimo elemento
+#print(bicycles[-1])
+
+# mudando o elemento
+bicycles[0] = 'caloi'
+#print(bicycles)
+
+# removendo mantendo o elemento
+print(bicycles.pop())
+print(bicycles)
